8b951584bc95c8f9bb305211c2ae2db555d18c1b
Vitaliy <ilinov123@ukr.net>
2021-12-15 22:28:13 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 var_dump(__FILE__);
 
-//file_put_contents('../test.txt', 'check permission' . PHP_EOL, FILE_APPEND);
+//file_put_contensdfmission' . PHP_EOL, FILE_APPEND);
 
 phpinfo();
